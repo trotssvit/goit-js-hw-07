3b70d498f38d2094e680de648d52955c3f70d4e1
Svitlana Trotsenko <trots.svit@gmail.com>
2023-07-20 00:08:47 +0100

    1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 
+// console.log(galleryItems)
+// const list = document.querySelector('.gallery')
+
+
+function createImageGallery(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('')
+
+}
+
+const galleryElList = document.querySelector('.gallery');
+
+galleryElList.insertAdjacentHTML('afterbegin', createImageGallery(galleryItems));
+
+galleryElList.addEventListener('click', selectImage);
+
+function selectImage(img) {
+    img.preventDefault()
+
+    if (!img.target.classList.contains("gallery__image")) {
+        return
+    }
+
+
+    const instance = basicLightbox.create(`
+    <img src="${img.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show()
+};
